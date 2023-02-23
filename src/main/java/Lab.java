5d11dfb8bd5267f8c4762c22bd93eaba3666fd2a
tuantrinh18 <tuantrinh1803@gmail.com>
2023-02-23 16:52:05 +0000

    labcompleted

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -1,5 +1,5 @@
 
-public class Lab {
+public class Lab extends RuntimeException{
     /**
      * This method must throw an unchecked exception.
      * unchecked means that you do not need to wrap the method in a try/catch or a throws declaration.
@@ -13,7 +13,13 @@ public class Lab {
      * Notice that errors are separate from exceptions. Errors are external to Java and can occur when some external
      * event happens, like running out of computer memory.
      */
-    public void throwUncheckedException(){
+
+     
+    public int throwUncheckedException ()
+    {
+        throw new Lab();
+        
 
     }
+    
 }
